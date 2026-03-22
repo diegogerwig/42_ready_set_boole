2a10feff8f3413719fe844fe04eab6da0029bca4
Diego Gerwig <dgerwig-@student.42urduliz.com>
2026-03-22 19:51:42 +0100

    update

--- tests/test_ex11.py
+++ tests/test_ex11.py
@@ -2,6 +2,9 @@ from ex11_inverse import inverse_curve
 from ex10_curve import map_to_curve 
 from utils import *
 
+class PrettyFloat(float):
+    def __repr__(self):
+        return f"{self:.15f}"
 
 def run():
     print_header(11, "INVERSE CURVE (REVERSE Z-ORDER)")
@@ -10,15 +13,15 @@ def run():
 
     cases = [
         # ((float_value,), (expected_x, expected_y))
-        ((0.0,), (0, 0)),
-        (((1.0 / MAX_VAL),), (1, 0)),
-        (((2.0 / MAX_VAL),), (0, 1)),
-        (((3.0 / MAX_VAL),), (1, 1)),
-        (((4.0 / MAX_VAL),), (2, 0)),
-        (((8.0 / MAX_VAL),), (0, 2)),
-        (((12.0 / MAX_VAL),), (2, 2)),
-        (((15.0 / MAX_VAL),), (3, 3)),
-        ((1.0,), (0xFFFF, 0xFFFF)), 
+        ((PrettyFloat(0.0),), (0, 0)),
+        ((PrettyFloat(1.0 / MAX_VAL),), (1, 0)),
+        ((PrettyFloat(2.0 / MAX_VAL),), (0, 1)),
+        ((PrettyFloat(3.0 / MAX_VAL),), (1, 1)),
+        ((PrettyFloat(4.0 / MAX_VAL),), (2, 0)),
+        ((PrettyFloat(8.0 / MAX_VAL),), (0, 2)),
+        ((PrettyFloat(12.0 / MAX_VAL),), (2, 2)),
+        ((PrettyFloat(15.0 / MAX_VAL),), (3, 3)),
+        ((PrettyFloat(1.0),), (0xFFFF, 0xFFFF)), 
         
         # Casos de error
         ((-0.1,), None),
